9b8daecdeb793ba800c754a81f34dad9771b160a
Logan Campbell <colin@logaan.net>
2023-09-22 00:13:33 +1000

    doctype and html tag

--- src/logan_cam/core.clj
+++ src/logan_cam/core.clj
@@ -1,8 +1,15 @@
+;; 55 lines per page
+;; 3 char left margin. 69 chars content.
 (ns logan-cam.core
-  (:require [hiccup.core :as h]))
+  (:require [hiccup.core :as c]
+            [hiccup.page :as p]))
 
 (defn foo []
-  (h/html [:h1 "Hello, World!"]))
+  (c/html
+   (list
+    (p/doctype :xhtml-transitional)
+    (p/xhtml-tag "en"
+                 [:h1 "Hello, World!"]))))
 
 (defn render-and-write [path]
   (spit path (foo)))
